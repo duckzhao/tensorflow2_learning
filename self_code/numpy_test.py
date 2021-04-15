aee58bb11575ab93b50fdc16e7c7e2349c55be13
1161678627 <1161678627@qq.com>
2021-04-15 11:44:19 +0800

    新增部分笔记内容

--- self_code/numpy_test.py
+++ self_code/numpy_test.py
@@ -4,3 +4,8 @@ w = np.arange(12).reshape(3, 4)
 b = np.array([1, 2, 3, 4])
 print(w)
 print(w+b - w)
+
+
+a = np.arange(12).reshape(3, 4)
+print(a)
+print(a.max())
--- self_code/tf_often_used_func_02.py
+++ self_code/tf_often_used_func_02.py
@@ -6,14 +6,15 @@ import tensorflow as tf
 import numpy as np
 
 # 1.使用tf.GradientTape 计算某个变量的梯度/偏导---计算时候都是返回的数字结果，不是带有公式的那种，求导结果也都是数字
-# 在 tf.GradientTape() 的上下文内，“所有计算步骤”都会被记录以用于求导
+# GradientTape是一个求导记录器，在 tf.GradientTape() 的上下文内，“所有的函数计算步骤”都会被记录以用于求导
+# 如果要计算损失函数的倒数（梯度），就要把损失函数的自定义计算步骤，完整的写在tape内，才可以求导并且自更新
 with tf.GradientTape() as tape:
     # 定义一个变量w，且赋予初值.变量与普通张量的一个重要区别是其默认能够被 TensorFlow 的自动求导机制所求导，因此往往被用于定义机器学习模型的参数
-    w = tf.Variable(tf.constant(value=3.))  # 可以直接写 3.
+    w = tf.Variable(tf.constant(value=3.))  # 只有变量才可以被tape求导记录
     # 定义一个损失函数loss，为w平方
     loss = tf.square(w)  # 会返回一个当前点的数值型loss值
-    # 计算loss函数对当前w值求偏导的结果---即当前loss函数关于w的梯度, target目标函数，sources求导变量
-    grad = tape.gradient(target=loss, sources=w)
+    # 计算loss函数对当前w值求偏导的结果---即当前loss函数关于w变量的梯度, target目标函数，sources求导变量
+    grad = tape.gradient(target=loss, sources=w)    # w变量是和loss函数有关的变量，在tape记录的loss函数形式中对变量w求导
 print('变量w=3.时在函数w^2中求导数的结果为：', grad)
 
 
@@ -27,6 +28,35 @@ with tf.GradientTape() as tape:
 w_grad, b_grad = tape.gradient(L, [w, b])   # 以列表形式传入变量，则返回多个偏导结果->代入一个变量的当前值，求另一个变量的当前点偏导值
 print(L, w_grad, b_grad)
 
+
+# tensorflow手写自定义的梯度下降及参数自更新,使用线性模型y=aX+b拟合以下数据
+X_raw = np.array([2013, 2014, 2015, 2016, 2017], dtype=np.float32)
+y_raw = np.array([12000, 14000, 15000, 16500, 17500], dtype=np.float32)
+# 归一化
+X = (X_raw - X_raw.min())/(X_raw.max() - X_raw.min())
+y = (y_raw - y_raw.min())/(y_raw.max() - y_raw.min())
+
+# 将x，y转为tf张量
+X = tf.constant(X)
+y = tf.constant(y)
+# 使用 tape.gradient(ys, xs) 自动计算梯度；
+# 使用 optimizer.apply_gradients(grads_and_vars) 自动更新模型参数。---->也可以使用assign_sub完成
+a = tf.Variable(initial_value=0.)
+b = tf.Variable(initial_value=0.)  # 梯度下降变量
+
+num_epochs = 10000  # 迭代轮数
+# 优化器可以帮助我们根据计算出的求导结果,以一定的学习率更新模型参数，从而最小化某个特定的损失函数，具体使用方式是调用其 apply_gradients() 方法
+optimizer = tf.keras.optimizers.SGD(learning_rate=5e-4)  # 控制梯度下降更新方式的优化器
+# 开始迭代训练
+for index in range(num_epochs):
+    # 使用tf.GradientTape记录loss函数的内容
+    with tf.GradientTape() as tape:
+        y_pred = a * X + b  # 预测值
+        loss = tf.reduce_sum(input_tensor=tf.square(y_pred - y))    # 损失函数有了
+    a_grad, b_grad = tape.gradient(target=loss, sources=[a, b])  # 偏导数有了    tape可以脱离with域作用，显得代码结构更清晰
+    # 使用optimizer.apply_gradients优化器完成参数自更新,传入一个list，list中的元素是tuple->（变量的偏导数，变量）
+    optimizer.apply_gradients(grads_and_vars=[(a_grad, a), (b_grad, b)])    # 原地更新，无需返回
+print('优化后的参数分别为：', a.numpy(), b.numpy())
 
 # 2.one_hot编码，主要用于将label列，转换为矩阵形式表示，label所在编码值为1，其余值为0，矩阵每行仅有1个1，表示当前label
 classes = 3  # 编码深度/类别数
